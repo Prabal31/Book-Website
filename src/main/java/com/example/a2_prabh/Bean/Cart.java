5b68f757a7143712efeabe16bf9cca182cf14bfc
Prabal <manchandaprabal878@gmail.com>
2023-11-16 13:06:26 -0500

    Created a user class in bean

---
+++ src/main/java/com/example/a2_prabh/Bean/Cart.java
@@ -0,0 +1,19 @@
+package com.example.a2_prabh.Bean;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+
+
+public class Cart {
+
+    private String title;
+    private String author;
+    private String ISBN;
+    private double price;
+    private String description;
+
+
+}
