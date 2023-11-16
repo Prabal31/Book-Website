f0603093dfacd956581f2961314a01cba91e5dff
Prabal <manchandaprabal878@gmail.com>
2023-11-16 14:03:03 -0500

    user post maping for inserting books in cart

--- src/main/java/com/example/a2_prabh/Controller/HomeController.java
+++ src/main/java/com/example/a2_prabh/Controller/HomeController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
@@ -17,25 +18,22 @@ public class HomeController {
     @Autowired
     DataBaseAccess da;
     List<Book> bookList = new CopyOnWriteArrayList<Book>();
-    List<Book> cartList = new CopyOnWriteArrayList<Book>();
-
-
 
     @GetMapping("/")
     public String Books(Model model) {
         List<Book> bookList =da.getbook();
         model.addAttribute("bookList", da.getbook());
-        System.out.println("LOL");
         return "index";
     }
 
-    @GetMapping("/addToCart/{id}")
+    @PostMapping("/addToCart/{id}")
     public String addToCart(@PathVariable int id) {
+        System.out.println("LOL");
 
         Book book = da.getBookByID(id);
 
         da.insertBookInCart(book);
 
-        return "itemAdded";
+        return "index";
     }
 }
